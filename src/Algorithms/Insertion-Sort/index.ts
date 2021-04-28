c3cc028911b30b3b70fccecb6aa1b14338c8d6a3
roman <sk.trac@yandex.ru>
2021-04-28 12:58:16 +0300

    * working on insertion sort

---
+++ src/Algorithms/Insertion-Sort/index.ts
@@ -0,0 +1,16 @@
+/**
+ * @param {Array} arr
+ * @return {Array}
+ */
+export const insertionSort = (arr: number[]): number[] => {
+    const result = [...arr];
+    let currentSortedIndex = 0;
+    let nextIndex = currentSortedIndex + 1;
+    let isSorted = false;
+
+    do {
+
+    } while (isSorted)
+
+    return result;
+}
---
+++ src/Algorithms/Insertion-Sort/test.ts
@@ -0,0 +1,9 @@
+import {insertionSort} from "./index";
+
+describe("bubble sort", () => {
+    it("should sort correctly", () => {
+        const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+        const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        expect(insertionSort(nums)).toEqual(expectedResult);
+    });
+});
