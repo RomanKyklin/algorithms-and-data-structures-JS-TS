787c38e5a7843754d7c6c217fdb728364de77aed
roman <sk.trac@yandex.ru>
2021-04-28 18:18:48 +0300

    * working on insertion sort

--- src/Algorithms/Insertion-Sort/index.ts
+++ src/Algorithms/Insertion-Sort/index.ts
@@ -3,14 +3,34 @@
  * @return {Array}
  */
 export const insertionSort = (arr: number[]): number[] => {
+    if (arr.length <= 1) return arr;
+
     const result = [...arr];
     let currentSortedIndex = 0;
-    let nextIndex = currentSortedIndex + 1;
-    let isSorted = false;
+    let currentNonSortedIndex = 1;
+    let nonSorted = true;
 
     do {
-
-    } while (isSorted)
-
+        if (result[currentNonSortedIndex] != null) {
+            for (let i = currentSortedIndex; i >= 0; i--) {
+                if (result[currentNonSortedIndex] < result[i]) {
+                    let tmp = result[currentNonSortedIndex];
+                    result[currentNonSortedIndex] = result[i];
+                    result[i] = tmp;
+                    console.log('if statement', result);
+                } else {
+                    currentSortedIndex++;
+                    currentNonSortedIndex++;
+                    break;
+                }
+            }
+        } else {
+            console.log('sorted')
+            nonSorted = false;
+        }
+    } while (nonSorted)
+    console.log('return result');
     return result;
 }
+
+console.log(insertionSort([10, 5, 3, 8, 2, 6, 4, 7, 9, 1]));
