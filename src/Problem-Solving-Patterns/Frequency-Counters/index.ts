4bb84bc76a8843e8389c5bf4059281025cca2fa0
roman <sk.trac@yandex.ru>
2021-05-02 16:43:00 +0300

    * frequency counter pattern example added naive solution

--- src/Problem-Solving-Patterns/Frequency-Counters/index.ts
+++ src/Problem-Solving-Patterns/Frequency-Counters/index.ts
@@ -9,10 +9,26 @@
  * has it's corresponding value squared in the second array. The frequency of values must be the same.
  */
 
+/**
+ * A Naive solution with Time Complexity - O(n^2)
+ * @return {boolean}
+ * @param {Array<number>} arr1
+ * @param {Array<number>} arr2
+ */
 const same = (arr1: number[], arr2: number[]): boolean => {
-    return true;
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        let correctIndex = arr2.indexOf(arr1[i] ** 2)
+        if (correctIndex === -1) {
+            return false;
+        }
+        arr2.splice(correctIndex, 1)
+    }
+    return true
 }
 
-same([1, 2, 3], [4, 1, 9]) // true
-same([1, 2, 3], [1, 9]) // false
-same([1, 2, 1], [4, 4, 1]) // false (must be same frequency)
+console.log(same([1, 2, 3], [4, 1, 9])) // true
+console.log(same([1, 2, 3], [1, 9])) // false
+console.log(same([1, 2, 1], [4, 4, 1]))// false (must be same frequency)
