1f07d7cca72c46207d5b9aca8ff4abd009922e35
roman <sk.trac@yandex.ru>
2021-05-02 17:55:32 +0300

    * sumZeroFrequencyPattern blueprint added

--- src/Problem-Solving-Patterns/Multiple-Pointers/index.ts
+++ src/Problem-Solving-Patterns/Multiple-Pointers/index.ts
@@ -16,6 +16,10 @@ const sumZero = (arr: number[]): number[] => {
     return [];
 }
 
+const sumZeroFrequencyPattern = (arr: number[]): number[] => {
+    return [];
+}
+
 sumZero([-3, -2, -1, 0, 1, 2, 3]) // [-3,3]
 sumZero([-2, 0, 1, 3]) // undefined
 sumZero([1, 2, 3]) // undefined
