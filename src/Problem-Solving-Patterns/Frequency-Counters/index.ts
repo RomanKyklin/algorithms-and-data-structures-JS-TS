96367b0f0888b328b2285ab7bfe832c628a3c1e0
roman <sk.trac@yandex.ru>
2021-05-02 16:35:28 +0300

    * frequency counter pattern description added

---
+++ src/Problem-Solving-Patterns/Frequency-Counters/index.ts
@@ -0,0 +1,5 @@
+/*
+ * This pattern uses objects or sets to collect values / frequencies of value
+ * This can often avoid the need for nested loops or O(n^2) operations with arrays / strings
+ */
+
