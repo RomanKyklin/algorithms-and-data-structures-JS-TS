0907f1c892cd37e8e2a6990b80740302affa022e
roman <sk.trac@yandex.ru>
2021-05-03 12:01:55 +0300

    added blueprint for countUniqueValues with multiple pointers

--- src/Problem-Solving-Patterns/Multiple-Pointers/index.ts
+++ src/Problem-Solving-Patterns/Multiple-Pointers/index.ts
@@ -51,11 +51,33 @@ const sumZeroFrequencyPattern = (arr: number[]): number[] => {
     }
 }
 
+/**
+ * EXAMPLE 2:
+ * Implement a function called countUniqueValues, which accepts a sorted array,
+ * and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted
+ */
+
+/**
+ * Naive solution
+ * @return {number}
+ * @param {Array<number>} arr
+ */
 const countUniqueValues = (arr: number[]): number => {
+    const result: number[] = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!result.includes(arr[i])) result.push(arr[i]);
+    }
+    return result.length;
+}
+
+const countUniqueValuesMultiplePointers = (arr: number[]): number => {
+    let result = 0;
+
     return 1;
 }
 
-countUniqueValues([1, 1, 1, 1, 1, 2]) // 2
-countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]) // 7
-countUniqueValues([]) // 0
-countUniqueValues([-2, -1, -1, 0, 1]) // 4
+console.log(countUniqueValues([1, 1, 1, 1, 1, 2])) // 2
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])) // 7
+console.log(countUniqueValues([])) // 0
+console.log(countUniqueValues([-2, -1, -1, 0, 1])) // 4
