54ce162a4a78400c76da07f8a04fe644b6270709
roman <sk.trac@yandex.ru>
2021-05-03 12:17:40 +0300

    added blueprint function and desription for sliding window pattern

---
+++ src/Problem-Solving-Patterns/Sliding-Window/index.ts
@@ -0,0 +1,27 @@
+/**
+ * TODO PATTERN DESCRIPTION:
+ * This pattern involves creating a window which can either be an array or number from one position to another
+ * Depending on a certain condition, the window either increases or closes (and a new window is created)
+ * Very useful for keeping track of a subset of data in an array/string etc.
+ **/
+
+/**
+ * TODO EXAMPLE #1
+ * Write a function called maxSubarraySum which accepts an array of integers and a number called n.
+ * The function should calculate the maximum sum of n consecutive elements in the array.
+ **/
+
+/**
+ * @return {number}
+ * @param {Array<number>} arr
+ * @param {number} num
+ */
+const maxSubarraySum = (arr: number[], num: number): number => {
+    return 1;
+}
+
+maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2) // 10
+maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4) // 17
+maxSubarraySum([4, 2, 1, 6], 1) // 6
+maxSubarraySum([4, 2, 1, 6, 2], 4) // 13
+maxSubarraySum([], 4) // null
