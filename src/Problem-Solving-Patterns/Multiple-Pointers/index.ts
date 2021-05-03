438cad2463150e4419c50e8dbd5e309f52a68000
roman <sk.trac@yandex.ru>
2021-05-03 11:49:25 +0300

    * refactored solution with multiple pointers pattern

--- src/Problem-Solving-Patterns/Multiple-Pointers/index.ts
+++ src/Problem-Solving-Patterns/Multiple-Pointers/index.ts
@@ -29,10 +29,28 @@ const sumZero = (arr: number[]): number[] => {
     }
 }
 
+/**
+ * Refactored solution with multiple pointers pattern
+ * Time Complexity - O(N)
+ * Space Complexity - O(1)
+ * @return {Array<number>}
+ * @param {Array<number>} arr
+ */
 const sumZeroFrequencyPattern = (arr: number[]): number[] => {
-    return [];
+    let left = 0;
+    let right = arr.length - 1;
+    while(left < right){
+        let sum = arr[left] + arr[right];
+        if(sum === 0){
+            return [arr[left], arr[right]];
+        } else if(sum > 0){
+            right--;
+        } else {
+            left++;
+        }
+    }
 }
 
-console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])) // [-3,3]
-console.log(sumZero([-2, 0, 1, 3])) // undefined
-console.log(sumZero([1, 2, 3])) // undefined
+console.log(sumZeroFrequencyPattern([-3, -2, -1, 0, 1, 2, 3])) // [-3,3]
+console.log(sumZeroFrequencyPattern([-2, 0, 1, 3])) // undefined
+console.log(sumZeroFrequencyPattern([1, 2, 3])) // undefined
