d2319d3b21f652b0837953edbc9877132270ee6d
roman <sk.trac@yandex.ru>
2021-05-04 21:15:43 +0300

    * linear search function description added

--- src/Algorithms/Linear-Search/index.ts
+++ src/Algorithms/Linear-Search/index.ts
@@ -1,3 +1,9 @@
+/**
+ * Linear Search Algorithm implementation
+ * BigO - worst O(n), best O(1)
+ * @param {Array<*>} arr
+ * @param {*} search
+ */
 export const linearSearch = (arr: any[], search: any): number | null => {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === search) {
