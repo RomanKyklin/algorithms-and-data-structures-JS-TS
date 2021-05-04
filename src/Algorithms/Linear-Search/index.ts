dc25729cd9217269b3bb98ebc08a5bda677378aa
roman <sk.trac@yandex.ru>
2021-05-04 21:11:35 +0300

    * linear search function added with tests

--- src/Algorithms/Linear-Search/index.ts
+++ src/Algorithms/Linear-Search/index.ts
@@ -1,3 +1,9 @@
-export const linearSearch = (arr: number[]): number[] => {
-    return [];
+export const linearSearch = (arr: any[], search: any): number | null => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === search) {
+            return i;
+        }
+    }
+
+    return null;
 }
--- src/Algorithms/Linear-Search/test.ts
+++ src/Algorithms/Linear-Search/test.ts
@@ -3,6 +3,9 @@ import {linearSearch} from "./index";
 describe("linear search", () => {
     it("should search correctly", () => {
         const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
-        const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const strings = ['a', 'b', 'c', 'd', 'f', 'g'];
+        expect(linearSearch(nums, 1)).toEqual(9);
+        expect(linearSearch(strings, 'd')).toEqual(3);
+        expect(linearSearch(strings, '23131')).toEqual(null);
     });
 });
