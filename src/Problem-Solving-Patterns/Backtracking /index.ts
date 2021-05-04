e6113eb51242943dbf39041c5182060546ae169a
roman <sk.trac@yandex.ru>
2021-05-04 16:22:59 +0300

    backtracking explanation added

---
+++ src/Problem-Solving-Patterns/Backtracking /index.ts
@@ -0,0 +1,21 @@
+/**
+ * TODO EXPLANATION
+ * Backtracking is a technique based on algorithm to solve problem. It uses recursive calling to find the solution by
+ * building a solution step by step increasing values with time. It removes the solutions that doesn't give rise
+ * to the solution of the problem based on the constraints given to solve the problem.
+ * Backtracking algorithm is applied to some specific types of problems
+ * 1. Decision problem used to find a feasible solution of the problem.
+ * 2. Optimisation problem used to find the best solution that can be applied.
+ * 3. Enumeration problem used to find the set of all feasible solutions of the problem
+ **/
+
+/**
+ * TODO ALGORITHMS EXAMPLES
+ * 1. MERGE SORT
+ * 2. BINARY SEARCH
+ * 3. Quick Sort
+ * 4. Fast Exponentiation
+ * 5. Strassen’s Algorithm
+ * 6. Kasturba Algorithm
+ * 7. Closest Pair of Points
+ */
