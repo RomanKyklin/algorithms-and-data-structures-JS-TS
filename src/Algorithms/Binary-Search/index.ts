a90142e24e882fa0a5d24e434e9d47111ac38d49
roman <sk.trac@yandex.ru>
2021-05-04 21:16:27 +0300

    * binary search function description added

--- src/Algorithms/Binary-Search/index.ts
+++ src/Algorithms/Binary-Search/index.ts
@@ -1,3 +1,9 @@
+/**
+ * Binary Search Algorithm implementation
+ * BigO - worst O(logN), best O(1)
+ * @param {Array<*>} arr
+ * @param {*} search
+ */
 export const binarySearch = (arr: any[], search: any): number | null => {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === search) {
