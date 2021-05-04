7b493b3508dad28a0da9c0c17485bfa0e9276e44
roman <sk.trac@yandex.ru>
2021-05-04 21:56:28 +0300

    * binary search implemented with tests

--- src/Algorithms/Binary-Search/index.ts
+++ src/Algorithms/Binary-Search/index.ts
@@ -1,15 +1,27 @@
 /**
  * Binary Search Algorithm implementation
- * BigO - worst O(logN), best O(1)
+ * BigO - worst and average O(logN), best O(1)
  * @param {Array<*>} arr
  * @param {*} search
  */
 export const binarySearch = (arr: any[], search: any): number | null => {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === search) {
-            return i;
+    let currentIndex = Math.floor(arr.length / 2);
+    let left = 0;
+    let right = arr.length - 1;
+
+    // 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+    while (arr[currentIndex] !== search && left <= right) {
+        if (arr[currentIndex] == null) return null;
+
+        if (arr[currentIndex] === search) return currentIndex;
+
+        if (arr[currentIndex] > search) {
+            right = currentIndex - 1;
+        } else if (arr[currentIndex] < search) {
+            left = currentIndex + 1;
         }
+        currentIndex = Math.floor((left + right) / 2);
     }
 
-    return null;
+    return arr[currentIndex] === search ? currentIndex : null;
 }
--- src/Algorithms/Binary-Search/test.ts
+++ src/Algorithms/Binary-Search/test.ts
@@ -2,9 +2,9 @@ import {binarySearch} from "./index";
 
 describe("binary search", () => {
     it("should search correctly", () => {
-        const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+        const nums = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
         const strings = ['a', 'b', 'c', 'd', 'f', 'g'];
-        expect(binarySearch(nums, 1)).toEqual(9);
+        expect(binarySearch(nums, 15)).toEqual(5);
         expect(binarySearch(strings, 'd')).toEqual(3);
         expect(binarySearch(strings, '23131')).toEqual(null);
     });
