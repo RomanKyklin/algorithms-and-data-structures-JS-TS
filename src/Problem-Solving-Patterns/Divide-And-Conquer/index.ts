0d35fc850b3b9f9faff46c4da062dd7d53699698
roman <sk.trac@yandex.ru>
2021-05-04 16:17:54 +0300

    divide and conquer description added

---
+++ src/Problem-Solving-Patterns/Divide-And-Conquer/index.ts
@@ -0,0 +1,20 @@
+/**
+ * TODO EXPLANATION
+ * In Computer Science, a divide and conquer is a type of algorithm designed to break down a problem into sub-problems
+ * which are usually simple enough to be solved directly. These solution to sub-problems can be combined to give our final answer.
+ * The Divide and Conquer technique can be understood as three parts :
+ * Divide —Dividing the problem into sub-problems
+ * Conquer — Conquering the sub-problems by solving them recursively.
+ * Combine — Combining all the solutions to sub-problems and merging them into solution of our original problem.
+ **/
+
+/**
+ * TODO ALGORITHMS EXAMPLES
+ * 1. MERGE SORT
+ * 2. BINARY SEARCH
+ * 3. Quick Sort
+ * 4. Fast Exponentiation
+ * 5. Strassen’s Algorithm
+ * 6. Kasturba Algorithm
+ * 7. Closest Pair of Points
+ */
