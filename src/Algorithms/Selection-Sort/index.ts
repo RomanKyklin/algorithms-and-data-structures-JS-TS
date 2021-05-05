08981c0d6a4ea90b593026b725cd9db86514f3cb
roman <sk.trac@yandex.ru>
2021-05-05 16:26:33 +0300

    * working on selection sort

--- src/Algorithms/Selection-Sort/index.ts
+++ src/Algorithms/Selection-Sort/index.ts
@@ -1,1 +1,21 @@
-export const selectionSort = (arr: number[]): number[] => arr;
+/**
+ * Selection sort algorithm implementation
+ * @return {Array<number>}
+ * @param {Array<number>} arr
+ */
+export const selectionSort = (arr: number[]): number[] => {
+    let isSwapped;
+    let min;
+    let sortedIndex;
+
+    do {
+        isSwapped = false;
+
+        for (let i = 0; i < arr.length; i++) {
+
+        }
+
+    } while (isSwapped)
+
+    return [];
+}
