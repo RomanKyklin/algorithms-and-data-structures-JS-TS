422754e019838d906a86c4e94997977ebd272137
roman <sk.trac@yandex.ru>
2021-05-05 16:50:48 +0300

    * bigO added

--- src/Algorithms/Insertion-Sort/index.ts
+++ src/Algorithms/Insertion-Sort/index.ts
@@ -1,37 +1,25 @@
 /**
+ * Insertion Sort algorithms implemented
+ * Overall time complexity of Insertion sort is O(n2)
  * @param {Array} arr
  * @return {Array}
  */
 export const insertionSort = (arr: number[]): number[] => {
     if (arr.length <= 1) return arr;
 
-    const result = [...arr];
-    let currentSortedIndex = 0;
-    let currentNonSortedIndex = 1;
-    let nonSorted = true;
+    let arrCopy = [...arr];
 
-    do {
-        if (result[currentNonSortedIndex] != null) {
-            for (let i = currentSortedIndex; i >= 0; i--) {
-                if (result[currentNonSortedIndex] < result[i]) {
-                    let tmp = result[currentNonSortedIndex];
-                    result[currentNonSortedIndex] = result[i];
-                    result[i] = tmp;
-                    console.log('iteration', i, result);
-                } else {
-                    currentSortedIndex++;
-                    currentNonSortedIndex++;
-                    break;
-                }
-            }
-            console.log('currentNonSortedIndex', currentNonSortedIndex, currentSortedIndex)
-        } else {
-            console.log('sorted')
-            nonSorted = false;
+    for (let i = 0; i < arrCopy.length; i++) {
+        let tmp = arrCopy[i];
+
+        for (var j = i - 1; j > -1 && arrCopy[i] > tmp; j--) {
+            arrCopy[j + 1] = arrCopy[j];
         }
-    } while (nonSorted)
-    console.log('return result');
-    return result;
+
+        arrCopy[j + 1] = tmp;
+    }
+
+    return arrCopy;
 }
 
 console.log(insertionSort([10, 5, 3]));
