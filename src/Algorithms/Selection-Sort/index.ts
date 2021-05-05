59e75af4a8938a540f59cbf6255f623d2af0617e
roman <sk.trac@yandex.ru>
2021-05-05 16:43:05 +0300

    * selection sort prints deleted

--- src/Algorithms/Selection-Sort/index.ts
+++ src/Algorithms/Selection-Sort/index.ts
@@ -26,7 +26,3 @@ export const selectionSort = (arr: number[]): number[] => {
 
     return arrCopy;
 }
-
-const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
-const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(selectionSort(nums))
