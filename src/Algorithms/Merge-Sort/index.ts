6e0e8e80db3e322f2f41e6b83b0ee30bff603725
roman <sk.trac@yandex.ru>
2021-05-06 18:58:34 +0300

    merge sort blueprint added

---
+++ src/Algorithms/Merge-Sort/index.ts
@@ -0,0 +1,22 @@
+/**
+ * Merge sort algorithms implemented
+ * Overall time complexity of Insertion sort is O(n2)
+ * @param {Array} arr
+ * @return {Array}
+ **/
+export const mergeSort = (arr: number[]): number[] => {
+    if (arr.length <= 1) return arr;
+
+    let arrCopy = [...arr];
+
+    for (let i = 1; i < arrCopy.length; i++) {
+
+        let k = i;
+        while (k > 0 && arrCopy[k - 1] > arrCopy[k]) {
+            [arrCopy[k], arrCopy[k - 1]] = [arrCopy[k - 1], arrCopy[k]];
+            k -= 1;
+        }
+    }
+
+    return arrCopy;
+}
