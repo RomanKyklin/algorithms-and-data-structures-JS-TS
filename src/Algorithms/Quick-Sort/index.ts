80b482e4841b6148e822cd1c1fed8d941c11a3ec
roman <sk.trac@yandex.ru>
2021-05-07 21:25:11 +0300

    quick sort blueprints

---
+++ src/Algorithms/Quick-Sort/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Quick Sort algorithms implemented
+ * Overall time complexity of Insertion sort is O(n2)
+ * @param {Array} arr
+ * @return {Array}
+ */
+export const quickSort = (arr: number[]): number[] => {
+    if (arr.length <= 1) return arr;
+
+
+    return [];
+}
+
+console.log(quickSort([10, 5, 3, 8, 2, 6, 4, 7, 9, 1]));
---
+++ src/Algorithms/Quick-Sort/test.ts
@@ -0,0 +1,9 @@
+import {quickSort} from "./index";
+
+describe("quick sort", () => {
+    it("should sort correctly", () => {
+        const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+        const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        expect(quickSort(nums)).toEqual(expectedResult);
+    });
+});
