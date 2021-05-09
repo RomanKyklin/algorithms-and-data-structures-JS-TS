35e6b10aba8c667c58a4453cca4191054f709cb3
roman <sk.trac@yandex.ru>
2021-05-09 14:05:42 +0300

    radix sort blueprint added

---
+++ src/Algorithms/Radix-Sort/index.ts
@@ -0,0 +1,3 @@
+export const radixSort = (arr: number[]): number[] => {
+    return [];
+}
---
+++ src/Algorithms/Radix-Sort/test.ts
@@ -0,0 +1,9 @@
+import {radixSort} from "./index";
+
+describe("radix sort", () => {
+    it("should sort correctly", () => {
+        const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+        const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        expect(radixSort(nums)).toEqual(expectedResult);
+    });
+});
