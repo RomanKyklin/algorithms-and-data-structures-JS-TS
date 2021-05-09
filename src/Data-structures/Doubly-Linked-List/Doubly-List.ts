8946e8748ce9ae7cfd5f3526e80d4cffb4e57ecc
roman <sk.trac@yandex.ru>
2021-05-09 16:33:35 +0300

    * push & get added

--- src/Data-structures/Doubly-Linked-List/Doubly-List.ts
+++ src/Data-structures/Doubly-Linked-List/Doubly-List.ts
@@ -12,12 +12,34 @@ export class DoublyList implements IList {
     delete(index: number): void {
     }
 
-    get(index: number): void {
+    get(index: number): IDoublyNode {
+        if (index === 0) return this.head;
+
+        let element: IDoublyNode = this.head.next;
+        let currentIndex = 1;
+
+        while (element != null) {
+            if (currentIndex === index) return element;
+
+            element = element.next;
+            currentIndex += 1;
+        }
+
+        return null;
     }
 
-    pop(): void {
+    pop(): IDoublyNode {
+        return this.head;
     }
 
-    push(element: any): void {
+    push(element: IDoublyNode): void {
+        if (!this.head) {
+            this.head = element;
+        } else {
+            this.tail.next = element;
+            element.previous = this.tail;
+        }
+        this.tail = element;
+        this.length += 1;
     }
 }
