f0e5f833abfb5fe067c96ae290848a52151de131
roman <sk.trac@yandex.ru>
2021-05-09 14:16:28 +0300

    radix sort getDigit helper added, tests implemented

--- src/Algorithms/Radix-Sort/index.ts
+++ src/Algorithms/Radix-Sort/index.ts
@@ -1,3 +1,11 @@
 export const radixSort = (arr: number[]): number[] => {
     return [];
 }
+
+export const getDigit = (number: number, position: number): number | null => {
+    let numberAtGivenPosition = String(number)[position - 1];
+
+    return numberAtGivenPosition != null ? +(String(number)[position - 1]) : null;
+}
+
+console.log(getDigit(100, 4));
--- src/Algorithms/Radix-Sort/test.ts
+++ src/Algorithms/Radix-Sort/test.ts
@@ -1,6 +1,13 @@
-import {radixSort} from "./index";
+import {getDigit, radixSort} from "./index";
 
 describe("radix sort", () => {
+    it("get digit helper test, should return number at given position", () => {
+        expect(getDigit(100, 1)).toEqual(1);
+        expect(getDigit(103, 2)).toEqual(0);
+        expect(getDigit(103, 3)).toEqual(3);
+        expect(getDigit(103, 4)).toEqual(null);
+    })
+
     it("should sort correctly", () => {
         const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
         const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
